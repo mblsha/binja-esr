5bfd5d647711b4530e69168349e733795b4ac11a
Michael Pishchagin <mblsha@gmail.com>
2025-06-07 15:15:13 +1000

    Use bundled lark package

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 import sys
+import os
 import importlib.util
+
+# Ensure bundled lark submodule is preferred over any installed version
+third_party = os.path.join(os.path.dirname(__file__), "third-party")
+if os.path.isdir(third_party) and third_party not in sys.path:
+    sys.path.insert(0, third_party)
 
 def module_exists(module_name):
     if module_name in sys.modules:
