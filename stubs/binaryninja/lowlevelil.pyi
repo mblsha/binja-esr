7d899dd1979bc2d9f2c215e6fe290dcf758e2646
Michael Pishchagin <mblsha@gmail.com>
2025-06-07 13:36:19 +1000

    Relax stubs for LowLevelIL

--- stubs/binaryninja/lowlevelil.pyi
+++ stubs/binaryninja/lowlevelil.pyi
@@ -1,9 +1,15 @@
-from typing import Any
+from typing import Any, Callable
 
 ExpressionIndex = int
 
+def LLIL_TEMP(n: int) -> ExpressionIndex:
+    ...
+
 class LowLevelILFunction:
     def expr(self, *args: Any, size: int | None, flags: Any | None = None) -> Any:
+        ...
+
+    def __getattr__(self, name: str) -> Callable[..., Any]:
         ...
 
 class LowLevelILLabel:
