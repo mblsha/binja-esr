00ed378a196f06d3c6e76798acbe8206eb32bbb6
Michael Pishchagin <mblsha@gmail.com>
2025-06-07 18:50:04 +1000

    Remove lark submodule

--- sc62015/__init__.py
+++ sc62015/__init__.py
@@ -1,7 +1,2 @@
-import os
-import sys
-
-third_party = os.path.join(os.path.dirname(os.path.dirname(__file__)), "third-party")
-if os.path.isdir(third_party) and third_party not in sys.path:
-    sys.path.insert(0, third_party)
+"""sc62015 package."""
 
