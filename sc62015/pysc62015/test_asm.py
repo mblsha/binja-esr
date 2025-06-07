d918696f05e96769260b04c1eb9edcbb26f34c99
Michael Pishchagin <mblsha@gmail.com>
2025-06-08 08:58:20 +1000

    Add register-register EX support

--- sc62015/pysc62015/test_asm.py
+++ sc62015/pysc62015/test_asm.py
@@ -813,6 +813,15 @@ assembler_test_cases: List[AssemblerTestCase] = [
             q
         """,
     ),
+    AssemblerTestCase(
+        test_id="ex_reg_reg_simple",
+        asm_code="EX X, Y",
+        expected_ti="""
+            @0000
+            ED 45
+            q
+        """,
+    ),
     # --- CMP Instruction Tests ---
     AssemblerTestCase(
         test_id="cmp_a_imm",
