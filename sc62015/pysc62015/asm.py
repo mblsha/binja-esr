a2694f3d3e97dbc5074ab2faf6c6913d90f4e7d3
Michael Pishchagin <mblsha@gmail.com>
2025-06-09 12:21:22 +1000

    Move Binary Ninja helpers to dedicated package

--- sc62015/pysc62015/asm.py
+++ sc62015/pysc62015/asm.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Optional, Union, cast, TypedDict, Type, Literal
-from . import binja_api  # noqa: F401
+from binja_helpers import binja_api  # noqa: F401
 from binaryninja.architecture import RegisterName
 from lark import Lark, Transformer, Token
 from .instr import (
