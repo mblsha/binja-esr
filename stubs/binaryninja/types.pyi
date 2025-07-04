dcb124d55cddd702184a2c3bcced5a078e77ea2f
Michael Pishchagin <mblsha+gh@gmail.com>
2025-07-04 14:15:55 +1000

    feat: Add Type class mock for array type creation

--- stubs/binaryninja/types.pyi
+++ stubs/binaryninja/types.pyi
@@ -6,3 +6,10 @@ class Symbol:
     name: str
     
     def __init__(self, symbol_type: Any, addr: int, name: str) -> None: ...
+
+class Type:
+    @staticmethod
+    def array(element_type: 'Type', count: int) -> 'Type': ...
+    
+    @staticmethod
+    def int(width: int, sign: bool = True, altname: str = "") -> 'Type': ...
