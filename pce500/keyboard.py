770250e87f420476c46958627408edece675c127
Michael Pishchagin <mblsha+gh@gmail.com>
2025-11-03 20:03:29 +1100

    Drop unused compat shims

--- pce500/keyboard.py
+++
@@ -1,8 +0,0 @@
-"""Public keyboard handler re-export."""
-
-from .keyboard_compat import (
-    DEFAULT_DEBOUNCE_READS,
-    PCE500KeyboardHandler,
-)
-
-__all__ = ["PCE500KeyboardHandler", "DEFAULT_DEBOUNCE_READS"]
