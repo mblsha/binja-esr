4f161da82925519480e6a6bf084a2cb3caf91b9c
Michael Pishchagin <mblsha+gh@gmail.com>
2025-12-14 13:41:17 +1100

    ci: fix perfetto interned fields and web type shim

---
+++ web/src/wasm_shims.ts
@@ -0,0 +1,8 @@
+declare module '$lib/wasm/pce500_wasm/pce500_wasm.js' {
+	export default function init(input?: unknown): Promise<void>;
+
+	export class Pce500Emulator {
+		constructor();
+		[key: string]: unknown;
+	}
+}
